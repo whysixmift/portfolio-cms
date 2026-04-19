3567ac2cc09a140e63831255e8614ab60a03f452
whysixmift <you@example.com>
2026-04-19 23:01:41 +0700

    revert prisma client initializer since env exists

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,8 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const db = globalForPrisma.prisma || new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL || "file:./dev.db",
-});
+export const db = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
