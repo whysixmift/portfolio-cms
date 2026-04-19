b777c47879d4256b4563abac82fbe921d3189b02
whysixmift <you@example.com>
2026-04-19 22:59:25 +0700

    fix prisma client initialization

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,6 +2,8 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const db = globalForPrisma.prisma || new PrismaClient();
+export const db = globalForPrisma.prisma || new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL || "file:./dev.db",
+});
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
