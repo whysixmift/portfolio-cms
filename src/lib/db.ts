d746658327d1a22fad1b97822cf91b823eec77ed
whysixmift <you@example.com>
2026-04-19 23:05:54 +0700

    bypass prisma 7 generated typescript bugs

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,11 +3,8 @@ import { PrismaClient } from "@prisma/client";
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const db = globalForPrisma.prisma || new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL || "file:./prod.db"
-    }
-  }
+  // @ts-ignore - Prisma 7 type definition is broken but runtime requires this
+  datasourceUrl: process.env.DATABASE_URL || "file:./prod.db"
 });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
