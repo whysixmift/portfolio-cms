799af0d6421fce131f15feb309d9da1455802331
whysixmift <you@example.com>
2026-04-19 23:04:16 +0700

    inject datasources connection explicitly

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,6 +2,12 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const db = globalForPrisma.prisma || new PrismaClient();
+export const db = globalForPrisma.prisma || new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL || "file:./prod.db"
+    }
+  }
+});
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
